686691b7f36aef1cbd0c909b2dd48be4ddcaaf0a
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-09 17:07:42 +0530

    Added login css

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 // Route to Login Page
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
+    res.sendFile(__dirname + '/public/login.html');
   });
 
 app.post('/login', (req, res) => {
